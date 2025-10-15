589efd9df32dafcdb30ae358dae65d1c7d900c11
Shreshth Sharma <97122887+Shreshth0109@users.noreply.github.com>
2025-10-15 20:47:32 +0530

    minor fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,10 @@ import authRouter from "./auth.routes";
 
 const importRoutings = (app: any) => {
     app.use("/api/auth", authRouter);   // Authentication Routes
+
     // app.use("/api/students", StudentRouter);  // Students API
     // app.use("/api/exams", ExamRouter);  // Exams API
-    app.use('/api/school', SchoolRouter); // ✅ Uncommented School Route
+    app.use('/api/school', SchoolRouter); //
     app.use('/api/classes', classRoutes);
     // app.use("/api/results", ResultRouter);  // Results API
 };
