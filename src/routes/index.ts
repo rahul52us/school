d94bc808df435d8bbafacfef15aceeb2cdc5b4fc
Shreshth Sharma <97122887+Shreshth0109@users.noreply.github.com>
2025-10-16 23:36:44 +0530

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,19 @@
+// importRoutings.ts
+
 import SchoolRouter from "./school.route";
 import classRoutes from "./class.routes";
+import superAdminRouter from "./superadmin.routes";
 import authRouter from "./auth.routes";
 
 const importRoutings = (app: any) => {
-    app.use("/api/auth", authRouter);   // Authentication Routes
+    app.use("/api/auth", authRouter);
 
-    // app.use("/api/students", StudentRouter);  // Students API
-    // app.use("/api/exams", ExamRouter);  // Exams API
-    app.use('/api/school', SchoolRouter); //
-    app.use('/api/classes', classRoutes);
-    // app.use("/api/results", ResultRouter);  // Results API
+    app.use("/api/superadmin", superAdminRouter);
+    app.use("/api/school", SchoolRouter);
+    app.use("/api/classes", classRoutes);
+
+    // app.use("/api/teachers", teacherRouter);
+    // app.use("/api/students", studentRouter);
 };
-
 
 export default importRoutings;
