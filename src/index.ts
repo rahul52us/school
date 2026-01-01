e75311bad5c1184185b8e27674abe05442a81fba
rahulkushwah <rahul52us@gmail.com>
2026-01-01 19:25:53 +0530

    handle changes

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
